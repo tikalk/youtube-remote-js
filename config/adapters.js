fd03d49ef1c2f492f388f0b3e1ef7536cfa5aaab
John Smith <assaf@youappi.com>
2013-12-24 13:55:27 +0200

    updated mongodb connection adapter

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -14,37 +14,11 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'mongo',
-
-  // In-memory adapter for DEVELOPMENT ONLY
-  memory: {
-    module: 'sails-memory'
-  },
-
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data IS preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
-
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  mysql: {
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
-  },
-
-  mongo: {
-      module   : 'sails-mongo',
-      host     : 'localhost',
-      port     : 27017,
-      user     : '',
-      password : '',
-      database : 'sails-social-auth'
-  }
+    // If you leave the adapter config unspecified
+    // in a model definition, 'default' will be used.
+    'default': 'mongo',
+    mongo: {
+        module: 'sails-mongo',
+        url: 'mongodb://nodejitsu:a9f376308689dd428465d80bdae486d4@alex.mongohq.com:10093/nodejitsudb111657141'
+    }
 };
